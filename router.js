72272cd50245fec0f790ab667b80f2f331283eb3
Rene Kabanda <renerugwiro@yahoo.fr>
2023-01-23 17:45:39 +0000

    added user endpoints to the router.

--- router.js
+++ router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { registerNewUser, logUserIn, logUserOut, updateUser, deleteUserAccount  } = require("./controllers/userController")
 const {
   addBook,
   getAllBooks,
@@ -8,6 +9,14 @@ const {
   removeBook,
 } = require("./controllers/bookController");
 
+// Auth endpoints
+router.post("/register", registerNewUser)
+router.post("/login", logUserIn)
+router.get("/logout", logUserOut)
+router.post("/users/:username", updateUser)
+router.post("/users/:username", deleteUserAccount)
+
+// CRUD endpoints
 router.post("/books", addBook);
 router.get("/books", getAllBooks);
 router.get("/books/:id", getBook);
